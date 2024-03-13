c06821c7c1e785c5654d0803edf25e4433d65e04
Glen Eder <glen@enfos.com>
2024-03-13 17:09:10 -0400

    drops java support as it never worked

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,16 +18,15 @@ return {
       ensure_installed = {
         "lua_ls",
         "bashls",
-        "jdtls",
       },
       -- define default handler setup method so that the lsps load 
       handlers = {
-         -- The first entry (without a key) will be the default handler
-         -- and will be called for each installed server that doesn"t have
-         -- a dedicated handler.
-         function (server_name) -- default handler (optional)
-             require("lspconfig")[server_name].setup {}
-         end,
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn"t have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup {}
+        end,
       },
     })
 
