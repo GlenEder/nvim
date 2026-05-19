de130a96157b9e9a2ffc7da84ea24c96d1a5dbc5
Glen Eder <geder@arraycorp.com>
2026-05-19 12:07:48 -0400

    fix: adds mjs file type

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { "vtsls", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "mjs" },
   root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
   log_level = vim.lsp.protocol.MessageType.Warning,
 }
